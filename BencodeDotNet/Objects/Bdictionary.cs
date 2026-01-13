0e8851a94f52c806095d620aa44cee7d694fe028
Jordi Vilaplana <jordiware@protonmail.com>
2026-01-13 12:13:25 +0100

    Implement IEquatable<Bdictionary> and override Equals, GetHashCode

--- BencodeDotNet/Objects/Bdictionary.cs
+++ BencodeDotNet/Objects/Bdictionary.cs
@@ -4,7 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Jordiware.BencodeDotNet.Objects;
 
-public sealed class Bdictionary : IBobject, IReadOnlyDictionary<Bstring, IBobject>
+public sealed class Bdictionary : IBobject, IReadOnlyDictionary<Bstring, IBobject>, IEquatable<Bdictionary>
 {
     private readonly ImmutableSortedDictionary<Bstring, IBobject> _keyValuePairs = ImmutableSortedDictionary<Bstring, IBobject>.Empty;
 
@@ -32,6 +32,28 @@ public sealed class Bdictionary : IBobject, IReadOnlyDictionary<Bstring, IBobjec
         return _keyValuePairs.GetEnumerator();
     }
 
+    public bool TryGetValue(Bstring key, [MaybeNullWhen(false)] out IBobject value)
+    {
+        return _keyValuePairs.TryGetValue(key, out value);
+    }
+
+    public bool Equals(Bdictionary? other)
+    {
+        if (other == null) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        if (!Enumerable.SequenceEqual(_keyValuePairs.Keys, other._keyValuePairs.Keys))
+            return false;
+
+        foreach (var key in _keyValuePairs.Keys)
+        {
+            var hasValue = other.TryGetValue(key, out var value);
+            if (!hasValue) return false;
+            if (!_keyValuePairs[key].Equals(value)) return false;
+        }
+        return true;
+    }
+
     public byte[] ToBinaryEncoding()
     {
         var encoded = new List<byte>([ Bencode.DictionaryBeginCharacter ]);
@@ -44,16 +66,27 @@ public sealed class Bdictionary : IBobject, IReadOnlyDictionary<Bstring, IBobjec
         return encoded.ToArray();
     }
 
-    public bool TryGetValue(Bstring key, [MaybeNullWhen(false)] out IBobject value)
-    {
-        return _keyValuePairs.TryGetValue(key, out value);
-    }
-
     IEnumerator IEnumerable.GetEnumerator()
     {
         return GetEnumerator();
     }
     #endregion
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Bdictionary other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        foreach (var kvp in _keyValuePairs)
+        {
+            hash.Add(kvp.Key);
+            hash.Add(kvp.Value);
+        }
+        return hash.ToHashCode();
+    }
 
     public override string ToString()
     {
