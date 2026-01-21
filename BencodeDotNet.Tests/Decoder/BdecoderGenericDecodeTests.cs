c75c21230747ba06e9c20aa32c9f1ab027b58664
Jordi Vilaplana <jordiware@protonmail.com>
2026-01-21 20:29:00 +0100

    Add tests for generic decoder

---
+++ BencodeDotNet.Tests/Decoder/BdecoderGenericDecodeTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Jordiware.BencodeDotNet.Tests.Decoder;
+
+public class BdecoderGenericDecodeTests
+{
+    [Theory]
+    [InlineData("i42e")]
+    public void DecodeUsesRegisteredSerializer(string input)
+    {
+        var decoder = new Bdecoder();
+        var bytes = Encoding.ASCII.GetBytes(input);
+
+        var result = decoder.Decode<int>(bytes);
+
+        Assert.Equal(42, result);
+    }
+
+    [Theory]
+    [InlineData("i42e")]
+    public void DecodeThrowsIfSerializerNotFound(string input)
+    {
+        var decoder = new Bdecoder();
+        var bytes = Encoding.ASCII.GetBytes(input);
+
+        Assert.Throws<NotSupportedException>(() => decoder.Decode<DateTimeOffset>(bytes));
+    }
+
+    [Theory]
+    [InlineData("i42e")]
+    public void DecodeThrowsIfDeserializationFails(string input)
+    {
+        var decoder = new Bdecoder();
+        var bytes = Encoding.ASCII.GetBytes(input);
+
+        Assert.Throws<InvalidOperationException>(() => decoder.Decode<string>(bytes));
+    }
+
+    [Theory]
+    [InlineData("i42e")]
+    public async Task DecodeAsyncThrowsIfCancelled(string input)
+    {
+        var decoder = new Bdecoder();
+        using var stream = new MemoryStream(Encoding.ASCII.GetBytes(input));
+        using var cts = new CancellationTokenSource();
+        cts.Cancel();
+
+        await Assert.ThrowsAsync<TaskCanceledException>(() => decoder.DecodeAsync<int>(stream, cts.Token));
+    }
+}
