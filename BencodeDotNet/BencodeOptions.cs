56bb9f9b5d86e67692fdcdaca5ccbbe3e1350072
Jordi Vilaplana <jordiware@protonmail.com>
2026-01-23 10:44:10 +0100

    Include text encoding in BencodeOptions

--- BencodeDotNet/BencodeOptions.cs
+++ BencodeDotNet/BencodeOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Jordiware.BencodeDotNet.Objects;
+using System.Text;
 
 namespace Jordiware.BencodeDotNet;
 
@@ -40,6 +41,23 @@ public struct BencodeOptions
     public const int DefaultMaxContainerItems = 1024;
 
     /// <summary>
+    /// The default text encoding used for Bencode string values.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This encoding is applied when no explicit <see cref="Encoding"/> is provided
+    /// to <see cref="BencodeOptions"/>. The default is <see cref="Encoding.UTF8"/>,
+    /// which aligns with the most common and widely supported encoding used in
+    /// Bencode implementations.
+    /// </para>
+    /// <para>
+    /// All string length calculations, serialization, and deserialization of
+    /// Bencode string values rely on this encoding unless overridden.
+    /// </para>
+    /// </remarks>
+    public static readonly Encoding DefaultTextEncoding = Encoding.UTF8;
+
+    /// <summary>
     /// Gets the maximum allowed nesting depth for Bencode containers.
     /// </summary>
     /// <remarks>
@@ -66,6 +84,22 @@ public struct BencodeOptions
     public readonly int MaxContainerItems;
 
     /// <summary>
+    /// The text encoding used for encoding and decoding Bencode string values.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This encoding determines how CLR <see cref="string"/> values are converted
+    /// to and from their byte representations when working with Bencode strings.
+    /// </para>
+    /// <para>
+    /// The value is immutable and is established when the <see cref="BencodeOptions"/>
+    /// instance is created. All string validation, size constraints, and decoding
+    /// behavior operate under this encoding.
+    /// </para>
+    /// </remarks>
+    public readonly Encoding TextEncoding;
+
+    /// <summary>
     /// Initializes a new <see cref="BencodeOptions"/> instance using the default constraint values.
     /// </summary>
     /// <remarks>
@@ -88,6 +122,11 @@ public struct BencodeOptions
     ///       <see cref="MaxContainerItems"/> is set to <see cref="DefaultMaxContainerItems"/>.
     ///     </description>
     ///   </item>
+    ///   <item>
+    ///     <description>
+    ///       <see cref="TextEncoding"/> is set to <see cref="DefaultTextEncoding"/>.
+    ///     </description>
+    ///   </item>
     /// </list>
     /// <para>
     /// This represents the recommended baseline configuration for general-purpose
@@ -95,10 +134,11 @@ public struct BencodeOptions
     /// </para>
     /// </remarks>
     public BencodeOptions()
+        : this(maxDepth: DefaultMaxDepth, 
+               maxPayloadLength: DefaultMaxPayloadLength, 
+               maxContainerItems: DefaultMaxContainerItems, 
+               textEncoding: DefaultTextEncoding)
     {
-        MaxDepth = DefaultMaxDepth;
-        MaxPayloadLength = DefaultMaxPayloadLength;
-        MaxContainerItems = DefaultMaxContainerItems;
     }
 
     /// <summary>
@@ -113,22 +153,29 @@ public struct BencodeOptions
     /// <param name="maxContainerItems">
     /// The maximum number of items allowed in any list or dictionary.
     /// </param>
+    /// <param name="textEncoding">
+    /// The text encoding used for encoding and decoding Bencode string values.
+    /// If <c>null</c>, <see cref="DefaultTextEncoding"/> is used.
+    /// </param>
     /// <remarks>
     /// <para>
     /// All parameters are optional and default to conservative, safe limits.
     /// </para>
     /// <para>
-    /// No validation is performed at construction time; invalid configurations
-    /// will be enforced when validation occurs.
+    /// The specified <paramref name="textEncoding"/> influences how string values
+    /// are converted to and from their byte representations, as well as how string
+    /// lengths are calculated for validation purposes.
     /// </para>
     /// </remarks>
     public BencodeOptions(int maxDepth = DefaultMaxDepth,
                           int maxPayloadLength = DefaultMaxPayloadLength,
-                          int maxContainerItems = DefaultMaxContainerItems)
+                          int maxContainerItems = DefaultMaxContainerItems,
+                          Encoding? textEncoding = null)
     {
         MaxDepth = maxDepth;
         MaxPayloadLength = maxPayloadLength;
         MaxContainerItems = maxContainerItems;
+        TextEncoding = textEncoding ?? DefaultTextEncoding;
     }
 
     /// <summary>
